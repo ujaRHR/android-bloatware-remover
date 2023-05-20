4115b15ad95186e2ff511f9e016a182cbc8118df
Mahmudul Hoque Khan <117527321+intelQong@users.noreply.github.com>
2023-05-20 18:44:53 +0600

    Update others.bash

--- Linux-macOS/others.bash
+++ Linux-macOS/others.bash
@@ -65,7 +65,7 @@ declare -a arr=(
     "com.ebay.carrier"
     "com.alibaba.aliexpresshd"
     "sg.bigo.live"
-    
+    "com.amazon.appmanager"
     
     ############################################################
     ## Add Package Name Here ##
